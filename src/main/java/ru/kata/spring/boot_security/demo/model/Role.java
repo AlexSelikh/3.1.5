1afc2897c94fc232098123b51a8a313b257645f0
AlexSelikh <alselikhov@gmail.com>
2023-02-06 21:23:00 +0300

    3.1.3x

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -5,7 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 import javax.persistence.*;
 import java.util.Set;
 
-@Entity
+
 @Table(name="roles")
 public class Role implements GrantedAuthority {
     @Id
